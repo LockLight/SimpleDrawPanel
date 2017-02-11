53ada78684b63d1f2724f089f054140ee603269f
LockLight <Locklightz@gmail.com>
2017-02-11 19:05:29 +0800

    Initial Commit

---
+++ 简易画板/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  简易画板
+//
+//  Created by locklight on 17/2/11.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 简易画板/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  简易画板
+//
+//  Created by locklight on 17/2/11.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
